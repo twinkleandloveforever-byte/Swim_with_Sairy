b82c52752de6c3758734505fc742ccc8a4ccb250
twinkleandloveforever-byte <twinkleandloveforever@gmail.com>
2026-06-26 20:53:33 -0500

    Modify names and reviews in reviewsGenerator.js
    Updated first names and last names arrays with new entries. Removed some review texts and added a few new ones.

--- src/utils/reviewsGenerator.js
+++ src/utils/reviewsGenerator.js
@@ -1,12 +1,15 @@
 const firstNames = [
-  'Daisy', 'George', 'Sayed', 'David', 'Emily', 'James', 'Ashley', 'Abdullah',
-  'Priyasha', 'Balaji', 'Nicole', 'Tyler', 'Raham', 'Brandon', 'Lauren', 'Justin',
-  'Zane', 'Aaron', 'Kim', 'Raj', 'Marcus', 'Brianna', 'Cameron', 'Jasmine',
-  'Devin', 'Alyssa', 'Zayed', 'Xiaomei', 'Young', 'Maya', 'Ming', 'Moe',
+  'Daisy', 'George', 'Jessica', 'David', 'Emily', 'James', 'Ashley', 'Chris',
+  'Amanda', 'Balaji', 'Nicole', 'Tyler', 'Megan', 'Brandon', 'Lauren', 'Justin',
+  'ayla', 'Aaron', 'Kim', 'Raj', 'Marcus', 'Brianna', 'Cameron', 'Jasmine',
+  'Devin', 'Alyssa', 'Zayed', 'Sofia', 'young', 'Maya', 'Logan', 'Mo',
 ];
 
 const lastNames = [
-  '',
+  'Zhang', '', '', 'Cao', 'Jones', '', 'Muhammad', 'Davis',
+  '', 'Liang', 'Hernandez', 'Raham', 'Gonzalez', 'Wilson', '',
+  'Zhen', 'Patel', 'Moore', 'Sadowski', 'Martin', 'Lee', 'Perez', 'Macguire',
+  '', 'Harris', '', '', 'Choi', 'Abdul', 'morris', 'Li',
 ];
 
 const reviewTexts = [
@@ -16,10 +19,6 @@ const reviewTexts = [
   'The mobile service is a game changer. No more rushing to pack up the kids and drive across town. The instructor came right to our home and was always on time.',
   'My daughter went from zero experience to joining the club swim team in one summer. The stroke refinement coaching was incredibly detailed and effective.',
   'Like it. awesome coach.'
-  'She is very energetic, my 9 yr old daughter loved her'
-  'I recommend Swim with Sairy for all you folks in Sugar Land area'
-  'love supporting local small businesses.'
-  'Sairy is great.'
 ];
 
 function pickRandom(arr) {
