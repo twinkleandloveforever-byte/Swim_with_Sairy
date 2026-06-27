199e8700b186e94e2ef3c4106bda0319045a780d
twinkleandloveforever-byte <twinkleandloveforever@gmail.com>
2026-06-27 10:24:16 -0500

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ function HeroSection() {
             <img
               src="https://i.postimg.cc/hPd5xdq7/Copilot-20260627-004810.png"
               alt="Swim with Sairy"
-              className="max-h-[16vh] sm:max-h-[20vh] md:max-h-[24vh] lg:max-h-[26vh] w-auto object-contain"
+              className="max-h-[16vh] sm:max-h-[40vh] md:max-h-[48vh] lg:max-h-[52vh] w-auto object-contain"
             />
           </div>
 
